87d0ff4f2b2b0242abebd8152b5c5360d3c44841
kelum <kelumnagodavithana1997@gmail.com>
2024-07-15 15:41:52 +0530

    initial commit

---
+++ src/main/java/com/crs/car_rental_system_SpringBoot/CarRentalSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.crs.car_rental_system_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystemSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crs/car_rental_system_SpringBoot/CarRentalSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crs.car_rental_system_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
